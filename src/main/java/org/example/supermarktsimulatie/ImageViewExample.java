775fa2a673712d50904eba3e25f06b5b066070f8
[Dion Stroet] <[dionstroet2@gmail.com]>
2025-09-12 12:09:41 +0200

    afbeelding is nu volledige grote

--- src/main/java/org/example/supermarktsimulatie/ImageViewExample.java
+++ src/main/java/org/example/supermarktsimulatie/ImageViewExample.java
@@ -13,29 +13,24 @@ public class ImageViewExample extends Application {
 
     @Override
     public void start(Stage stage) {
-        // Afbeelding laden vanuit resources
+
         Image image = new Image(getClass().getResourceAsStream("/afbeeldingen/farmershall_1stfloor.png"));
 
-        // ImageView maken en aspect ratio behouden
         ImageView imageView = new ImageView(image);
         imageView.setPreserveRatio(true);
 
-        // StackPane gebruiken om de afbeelding te centreren
         StackPane root = new StackPane(imageView);
 
-        // Schermgrootte ophalen
         Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
         double screenWidth = screenBounds.getWidth();
         double screenHeight = screenBounds.getHeight();
 
-        // Scene maken met schermgrootte
         Scene scene = new Scene(root, screenWidth, screenHeight);
 
-        // ImageView dynamisch schalen om het venster te vullen zonder witte ruimtes
         scene.widthProperty().addListener((obs, oldVal, newVal) -> scaleImage(imageView, scene));
         scene.heightProperty().addListener((obs, oldVal, newVal) -> scaleImage(imageView, scene));
 
-        // Stage instellen op schermgrootte
+        // schermgrootte
         stage.setTitle("ImageView Fill Window");
         stage.setX(screenBounds.getMinX());
         stage.setY(screenBounds.getMinY());
@@ -45,7 +40,7 @@ public class ImageViewExample extends Application {
         stage.setScene(scene);
         stage.show();
 
-        // Initiale schaal toepassen
+
         scaleImage(imageView, scene);
     }
 
@@ -56,7 +51,7 @@ public class ImageViewExample extends Application {
         double imageWidth = imageView.getImage().getWidth();
         double imageHeight = imageView.getImage().getHeight();
 
-        // Schaalfactor berekenen zodat het hele venster gevuld is (croppen indien nodig)
+
         double scale = Math.max(sceneWidth / imageWidth, sceneHeight / imageHeight);
 
         imageView.setFitWidth(imageWidth * scale);
